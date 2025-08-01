00bacd2e75b61adc2e47ff3f7bf37a6a9fcbab79
robertstoia <robertstoia@yahoo.com>
2025-08-01 09:52:41 +0300

    feat: display total price

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -20,11 +20,13 @@ class CartPage extends StatefulWidget {
 
 class _CartPageState extends State<CartPage> {
   late final List<CartListItem> _items;
+  int totalPrice = 0;
 
   @override
   void initState() {
     super.initState();
     _items = widget.items;
+    _calculateTotalPrice();
   }
 
   void addToCart(CartListItem item) {
@@ -32,6 +34,7 @@ class _CartPageState extends State<CartPage> {
       _items.add(item);
     });
 
+    _calculateTotalPrice();
     widget.onAddToCart(item);
   }
 
@@ -53,21 +56,49 @@ class _CartPageState extends State<CartPage> {
       });
     }
 
+    _calculateTotalPrice();
     widget.onRemoveFromCart(item);
   }
+
+  void _calculateTotalPrice() => totalPrice = _items.fold(
+        0,
+        (int price, item) => price + item.product.price,
+      );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(12),
-        child: ListView.builder(
-          itemCount: _items.length,
-          itemBuilder: (_, index) => CartListItemView(
-            item: _items[index],
-            onAddToCart: addToCart,
-            onRemoveFromCart: removeFromCart,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+              child: ListView.builder(
+                itemCount: _items.length,
+                itemBuilder: (_, index) => CartListItemView(
+                  item: _items[index],
+                  onAddToCart: addToCart,
+                  onRemoveFromCart: removeFromCart,
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.only(bottom: 20),
+              decoration: BoxDecoration(
+                border: Border.all(),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Row(
+                children: <Widget>[
+                  const Text("Total Price:"),
+                  const SizedBox(width: 5),
+                  Text("$totalPrice\$")
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
